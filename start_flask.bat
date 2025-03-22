44d76d34eacee9bdd1d7ac207ac3fa7d5ec3f6f3
Maisnerr <maisner.podolsky@gmail.com>
2025-03-22 22:06:30 +0100

    dodelavky

--- start_flask.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Users\freen\OneDrive\Plocha\lukas zaloha\webovky\url shortener
-py -m app
-cmd /k
--- start_ngrok.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Users\freen\Downloads\ngrok-v3-stable-windows-amd64
-ngrok http 5000
-cmd /k
